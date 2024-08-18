e69e28df388ed2588a53d895093c04590afea997
jivi3 <jivirockz1@gmail.com>
2024-08-18 15:17:32 -0400

    redirect

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Login from "./login.jsx";
 import Register from "./Register.jsx";
+import { Navigate } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
+				<Route path="/" element={<Navigate to="/login" replace />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/dashboard" element={<App />} />
 				<Route path="/register" element={<Register />} />
